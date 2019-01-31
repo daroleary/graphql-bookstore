73c938dbdbc2b2a1abecb5fc722fa79b042e2276
Darren O''Leary <darren.o@trufla.com>
2019-01-31 13:13:51 -0700

    Adding graphql query for all books and find book by id

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -11,12 +11,25 @@ module Types
       argument :id, ID, required: true
     end
 
+    field :book, BookType, null: true do
+      description "Find a book by id"
+      argument :id, ID, required: true
+    end
+
     def authors
       Author.all
     end
 
     def author(id:)
       Author.find(id)
+    end
+
+    def books
+      Book.all
+    end
+
+    def book(id:)
+      Book.find(id)
     end
   end
 end
