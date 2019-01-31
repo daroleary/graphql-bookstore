da38211648729c39dd02550b55c7705b63c5959a
Darren O''Leary <darren.o@trufla.com>
2019-01-31 13:07:03 -0700

    Adding graphql query for all and find author

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,22 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    description "root query"
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :authors, [AuthorType], null: true do
+      description "gets all authors"
+    end
+
+    field :author, AuthorType, null: true do
+      description "Find a author by id"
+      argument :id, ID, required: true
+    end
+
+    def authors
+      Author.all
+    end
+
+    def author(id:)
+      Author.find(id)
     end
   end
 end
